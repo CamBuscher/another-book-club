8cef2c49a67d4432b72a28504b384cb4ee39032d
Cameron Buscher <cameronbuscher@gmail.com>
2018-05-28 21:59:58 -0600

    setup redux, router, and eslint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './Components/App/App'
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+// const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider>
+    <Router>
+      <App />
+    </ Router>
+  </Provider>, document.getElementById('root'));
+
