0351575fa4b5fb40fc4778eb2be1a5533858f5f2
Cameron Buscher <cameronbuscher@gmail.com>
2018-05-29 20:52:30 -0600

    Fix app css and add some initial styling, with inevitable changes to come

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+import HomePage from '../HomePage/HomePage'
 import './App.css';
 import searchViaAuthorTitleISBN from '../../Helpers/APIcalls'
 
 class App extends Component {
   render() {
-    console.log(searchViaAuthorTitleISBN('annihilation'))
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Another book club</h1>
         </header>
+        <HomePage />
       </div>
     );
   }
