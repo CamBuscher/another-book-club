3034e0dad3ca38cbacc1629b83330f82891f798e
Cameron Buscher <cameronbuscher@gmail.com>
2018-05-29 22:29:48 -0600

    Book display now handles errors with no author or no preview image

--- src/Components/BooksDisplay/BooksDisplay.js
+++ src/Components/BooksDisplay/BooksDisplay.js
@@ -2,12 +2,23 @@ import React from 'react';
 
 export const BooksDisplay = ({books}) => {
   const bookPreviews = books.map(book => {
+    let previewImage;
+
+    !book.imageLinks ? 
+      previewImage = 'https://www.freeiconspng.com/uploads/no-image-icon-23.jpg' :
+      previewImage = book.imageLinks.smallThumbnail;
+
+    let author ;
+    
+    !book.authors ? 
+      author = 'Author not available' :
+      author = book.authors[0];
+
     return (
       <div key={book.publishedDate}>
         <h4>{book.title}</h4>
-        <h5>{book.subtitle}</h5>
-        <h6>Author: {book.authors[0]}</h6>
-        <img className='bookPreviewImg' src={book.imageLinks.smallThumbnail} />
+        <h5>Author: {author}</h5>
+        <img className='bookPreviewImg' src={previewImage} />
       </div>
     )
   });
