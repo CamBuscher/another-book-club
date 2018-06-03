f7c5ae5ec8a748ea69b83f1fd689fdf1d4b5f50c
Cameron Buscher <cameronbuscher@gmail.com>
2018-06-02 22:39:56 -0600

    Add createBookClub database method and update doCreateUser method

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -3,7 +3,15 @@ import { db } from './firebase';
 export const doCreateUser = (id, username, email) => 
   db.ref(`users/${id}`).set({
     username,
-    email
+    email,
+    id
+  });
+
+export const doCreateBookClub = (id, clubName, member) =>
+  db.ref(`bookClubs/${id}`).set({
+    clubName,
+    members: [...member],
+    id
   });
 
 export const onceGetUsers = () => 
