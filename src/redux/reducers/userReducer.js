cdb2e1af2922ea618d068aca02117a22c938ed6c
Cameron Buscher <cameronbuscher@gmail.com>
2018-06-03 20:26:30 -0600

    Clean up reducer

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -2,7 +2,6 @@ export const userReducer = (state = {}, action) => {
   switch (action.type) {
     case 'SET_USER':
       return action.user;
-
     case 'UPDATE_BOOK_CLUBS':
       return Object.assign(state, { bookClubs: { ...state.bookClubs, [action.clubName]: { id: action.club[action.clubName].id, clubName: action.club[action.clubName].clubName } } });
     default:
