75b78d7cf71704a95c83244c48da9572f7d09853
Cameron Buscher <cameronbuscher@gmail.com>
2018-06-05 00:28:04 -0600

    Add firebase comment capability

--- src/Components/WYSIWYG/Wysiwyg.js
+++ src/Components/WYSIWYG/Wysiwyg.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactQuill from 'react-quill';
+import { connect } from 'react-redux';
+import { db } from '../../firebase/';
 import 'react-quill/dist/quill.snow.css';
 import './Wysiwyg.css';
 
@@ -14,14 +16,25 @@ class Wysiwyg extends React.Component {
     this.setState({ text: value })
   }
 
-  handleComment = () => {
-    
+  handleComment = () => {  
+    if (!this.props.currentClub.comments) {
+      this.props.currentClub.comments = [];
+    }
+
+    const comment = {
+      user: this.props.user,
+      comment: this.state.text
+    };
+
+    db.addCommentToClub([...this.props.currentClub.comments, comment], this.props.currentClub.id);
+
+    this.setState({ text: '' });
   }
 
   render() {
     return (
       <div className='wysiwyg'>
-        <h5>Leave a comment!</h5>
+        <h4>Leave a comment!</h4>
         <ReactQuill value={this.state.text}
           onChange={this.handleChange} />
         <button 
@@ -32,4 +45,10 @@ class Wysiwyg extends React.Component {
   }
 }
 
-export default Wysiwyg;
+export const mapStateToProps = ({ currentClub, user }) => ({ currentClub, user });
+
+// export const mapDispatchToProps = dispatch => ({
+//   setUser: (user) => dispatch(setUser(user))
+// });
+
+export default connect(mapStateToProps)(Wysiwyg);
