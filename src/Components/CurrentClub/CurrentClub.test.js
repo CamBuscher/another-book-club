4ec0c21da5065a3379eeed860b15d2c0f3127d68
Cameron Buscher <cameronbuscher@gmail.com>
2018-06-06 02:40:36 -0600

    Change so you can leave any book club you're in

---
+++ src/Components/CurrentClub/CurrentClub.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CurrentClub from './CurrentClub';
+
+describe('CurrentClub', () => {
+  let wrapper;
+  beforeEach(() => {
+    // wrapper = shallow(<CurrentClub store={{getState: jest.fn()}} currentClub={{}}/>);
+  });
+
+  it('should match snapshot', () => {
+    // expect(wrapper).toMatchSnapshot();
+  });
+});
