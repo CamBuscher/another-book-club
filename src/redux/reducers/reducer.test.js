ea289a368633369b8abb68a4128cbe3855c24dce
Cameron Buscher <cameronbuscher@gmail.com>
2018-06-06 01:23:24 -0600

    Add reducers tests

--- src/redux/reducers/reducer.test.js
+++ src/redux/reducers/reducer.test.js
@@ -1,4 +1,6 @@
 import { currentClubReducer } from './currentClubReducer';
+import sessionReducer from './session';
+import {userReducer} from './userReducer';
 
 import * as actions from '../actions/actions';
 
@@ -31,6 +33,60 @@ describe('Current Club Reducer', () => {
     let expected = { comments: [{ name: 'cam' }] };
 
     let actual = currentClubReducer(undefined, actions.addCommentToClub({ name: 'cam' }));
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe('sessionReducer', () => {
+  it('should return an object with a null authUser if given no state', () => {
+    let expected = {authUser: null};
+
+    let actual = sessionReducer(undefined, { type: '@@INIT' });
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe('userReducer', () => {
+  it('should return an empty object if given no state', () => {
+    let expected = {};
+
+    let actual = userReducer(undefined, { type: '@@INIT' });
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('should return a user object whjen given correct action', () => {
+    let expected = {name: 'cam'};
+
+    let actual = userReducer({}, actions.setUser({name: 'cam'}));
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('should an updated club object whjen given correct action', () => {
+    let expected = { "bookClubs": { "cams club": { "clubName": "cams club", "id": "3" }}};
+
+    let actual = userReducer({}, actions.updateBookClubs({['cams club']: { clubName: 'cams club', id: '3', description: 'description' }}, 'cams club'));
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('should an updated clubs array whjen given correct action', () => {
+    let expected = {bookClubs: [{
+      book: 'hp'
+    }]};
+
+    let actual = userReducer({
+      bookClubs: {
+        camsClub: {
+          book: 'hp'
+        },
+        courtsClub: {
+          book: 'onyx'
+        }
+      }}, actions.removeBookClub('courtsClub'));
 
     expect(actual).toEqual(expected);
   });
