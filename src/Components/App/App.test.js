b41b322fa4e5b2dc3e5a9d9fbc362701b64fa4af
Cameron Buscher <cameronbuscher@gmail.com>
2018-06-05 21:33:06 -0600

    Add APP test for mapdispatchtoprops

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App, { mapDispatchToProps } from './App';
+import { setUser } from '../../redux/actions/actions';
 
+describe('App', () => {
+  it('calls dispatch with a setUser action', () => {
+    const dispatch = jest.fn();
 
+    const actionToDispatch = setUser();
+
+    const mappedProps = mapDispatchToProps(dispatch);
+
+    mappedProps.setUser();
+
+    expect(dispatch).toHaveBeenCalledWith(actionToDispatch);
+  });
+});
